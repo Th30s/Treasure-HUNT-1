39178f7f007f6167680374cb5ef920440647f19f
Th30s <43751800+Th30s@users.noreply.github.com>
2019-02-13 10:06:24 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ $(".oro").click(function() {
     $(".oro").text("JK YOU JUST GOT BEANED!!");
 });
 $("#scrtimg").mouseenter(function() {
-    $(".oro").text("doublle click the dragons pile of aurum");
+    $(".oro").text("Doublle click the dragons pile of AURUM");
 });    
+$("#aurum").dblclick(function() {
+    $("#gnome").fadeIn();
+});
+$("#reset").click(function() {
+    location.reload();
+});
