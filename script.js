eace41752934200e60a98b9840995e7b23c64fec
Th30s <43751800+Th30s@users.noreply.github.com>
2019-02-13 09:31:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".oro").click(function() {
+    $("#scrtimg").toggle();
+    $(".oro").text("JK YOU JUST GOT BEANED!!");
+});
+$("#scrtimg").mouseenter(function() {
+    $(".oro").text("doublle click the dragons pile of aurum");
+});    
